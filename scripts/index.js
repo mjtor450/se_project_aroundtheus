2da3397fc6cc3fd130f74e518a667948b4d34391
Moses T. <Moses.Torres33@yahoo.com>
2024-04-24 09:24:25 -0500

    adjusted

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 const initialCards = [
     {
-    title: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+        title: "Yosemite Valley",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
     },
     {
-    title: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+        title: "Lake Louise",
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
     },
     {
         title: "Bald Mountains",
