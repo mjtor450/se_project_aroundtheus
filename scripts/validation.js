5657397a2d973afbdffb9da30a5c95b0303f8df2
Moses T. <Moses.Torres33@yahoo.com>
2024-08-21 01:14:14 -0500

    validation.js added to code

---
+++ scripts/validation.js
@@ -0,0 +1,14 @@
+function enableValidation(config) {
+  console.log(config);
+}
+
+const config = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+enableValidation(config);
